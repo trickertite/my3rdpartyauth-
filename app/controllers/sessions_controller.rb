1d4f52bcead42409af89c9ce1b480a432104d2bf
trickertite <trickertite@gmail.com>
2015-04-19 22:18:51 +0300

    Initial Commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	# sleep 3
+  	redirect_to root_url
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
